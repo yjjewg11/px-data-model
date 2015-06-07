4ff17eb6404a7c028d2cf4c0a04d25bc5f4a312d
huxiaosc <292666766@qq.com>
2015-06-07 21:47:24 +0800

    清理代码

---
+++ src/main/java/com/company/news/jsonform/GroupRegJsonform.java
@@ -0,0 +1,63 @@
+package com.company.news.jsonform;
+
+
+
+/**
+ * 用户注册，body传入json数据转对象。
+ * 
+ * @author Administrator
+ * 
+ */
+public class GroupRegJsonform extends UserRegJsonform {
+
+	private String brand_name;// 品牌名称
+	private String company_name;// 公司全称
+	private String address;// 地址
+	private String map_point;// 坐标
+	private String link_tel;// 电话
+
+
+
+	public String getBrand_name() {
+		return brand_name;
+	}
+
+	public void setBrand_name(String brandName) {
+		brand_name = brandName;
+	}
+
+	public String getCompany_name() {
+		return company_name;
+	}
+
+	public void setCompany_name(String companyName) {
+		company_name = companyName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getMap_point() {
+		return map_point;
+	}
+
+	public void setMap_point(String mapPoint) {
+		map_point = mapPoint;
+	}
+
+	public String getLink_tel() {
+		return link_tel;
+	}
+
+	public void setLink_tel(String linkTel) {
+		link_tel = linkTel;
+	}
+
+
+
+}
