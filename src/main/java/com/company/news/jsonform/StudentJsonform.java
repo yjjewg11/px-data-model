da52ca97d4f2a9d35e8056db173c8eee0bc59a2e
yjjewg11 <yjjewg11>
2015-06-16 17:05:10 +0800

    update

--- src/main/java/com/company/news/jsonform/StudentJsonform.java
+++ src/main/java/com/company/news/jsonform/StudentJsonform.java
@@ -1,5 +1,8 @@
 package com.company.news.jsonform;
 
+import java.sql.Timestamp;
+
+
 
 
 /**
@@ -9,67 +12,210 @@ package com.company.news.jsonform;
  * 
  */
 public class StudentJsonform extends AbstractJsonform {
+	private String uuid;// 登录名。手机号码或邮箱
+	
+	public String getUuid() {
+		return uuid;
+	}
 
+
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+
+
+	private String loginname;// 登录名。手机号码或邮箱
+	
 	private String name;// 昵称
-
-	private String password;// 密码，md5加密。（UTF-8）区分大小写
-
-	private String tel;// 电话号码。【必填】
-
-	private String email;// email
-
-	private String group_uuid;// 关联组织UUID
 	
-	private Integer type;
+	private String password;// 密码，md5加密。（UTF-8）
 	
+	private String nickname;// 电话号码。
+	
+	private Integer sex;// 0:男,1:女
+	
+	private String headimg;// 用户类型。0:普通用户(默认)；1:组织管理员
+
+	
+	private String birthday;// email
 
 
-	public Integer getType() {
-		return type;
+	
+	private String ma_tel;// email
+
+	
+	private String ba_tel;// email
+
+	
+	private String nai_tel;// email
+
+	
+	private String ye_tel;// email
+
+	
+	private String waipo_tel;// email
+
+	
+	private String waigong_tel;// email
+
+	
+	private String other_tel;// email
+
+	
+	
+	public String getLoginname() {
+		return loginname;
 	}
 
-	public void setType(Integer type) {
-		this.type = type;
+
+	public void setLoginname(String loginname) {
+		this.loginname = loginname;
 	}
 
-	public String getGroup_uuid() {
-		return group_uuid;
-	}
-
-	public void setGroup_uuid(String groupUuid) {
-		group_uuid = groupUuid;
-	}
 
 	public String getName() {
 		return name;
 	}
 
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 
 	public String getPassword() {
 		return password;
 	}
 
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
-	public String getTel() {
-		return tel;
+
+	public String getNickname() {
+		return nickname;
 	}
 
-	public void setTel(String tel) {
-		this.tel = tel;
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
 	}
 
-	public String getEmail() {
-		return email;
+
+	public Integer getSex() {
+		return sex;
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
+
+	public void setSex(Integer sex) {
+		this.sex = sex;
 	}
 
+
+
+	public String getHeadimg() {
+		return headimg;
+	}
+
+
+	public void setHeadimg(String headimg) {
+		this.headimg = headimg;
+	}
+
+
+	public String getBirthday() {
+		return birthday;
+	}
+
+
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+
+
+	public String getMa_tel() {
+		return ma_tel;
+	}
+
+
+	public void setMa_tel(String ma_tel) {
+		this.ma_tel = ma_tel;
+	}
+
+
+	public String getBa_tel() {
+		return ba_tel;
+	}
+
+
+	public void setBa_tel(String ba_tel) {
+		this.ba_tel = ba_tel;
+	}
+
+
+	public String getNai_tel() {
+		return nai_tel;
+	}
+
+
+	public void setNai_tel(String nai_tel) {
+		this.nai_tel = nai_tel;
+	}
+
+
+	public String getYe_tel() {
+		return ye_tel;
+	}
+
+
+	public void setYe_tel(String ye_tel) {
+		this.ye_tel = ye_tel;
+	}
+
+
+	public String getWaipo_tel() {
+		return waipo_tel;
+	}
+
+
+	public void setWaipo_tel(String waipo_tel) {
+		this.waipo_tel = waipo_tel;
+	}
+
+
+	public String getWaigong_tel() {
+		return waigong_tel;
+	}
+
+
+	public void setWaigong_tel(String waigong_tel) {
+		this.waigong_tel = waigong_tel;
+	}
+
+
+	public String getOther_tel() {
+		return other_tel;
+	}
+
+
+	public void setOther_tel(String other_tel) {
+		this.other_tel = other_tel;
+	}
+
+
+
+
+	public String getClassuuid() {
+		return classuuid;
+	}
+
+
+	public void setClassuuid(String classuuid) {
+		this.classuuid = classuuid;
+	}
+
+
+	private String classuuid;// email
+	
 }
---
+++ src/main/java/com/company/news/vo/AnnouncementsVo.java
@@ -0,0 +1,102 @@
+package com.company.news.vo;
+
+import java.sql.Timestamp;
+
+
+
+public class AnnouncementsVo{
+	  private String uuid;//品牌名称
+	  
+	  private Timestamp create_time;//创建时间
+	  
+	  private String create_user;//品牌名称
+	  
+	  private String create_useruuid;//公司全称
+	  
+	  private Integer isimportant;//类型：'0:普通 1：重要',
+	  
+	  private String title;//坐标
+	  
+	  private String message;//电话
+	  
+	  private Integer type;//类型'0:普通通知 1:内部通知 2：班级通知',
+	  
+	  private String groupuuid;//坐标	 
+	  
+
+	  private String classuuids;
+	  
+	  public String getClassuuids() {
+		return classuuids;
+	}
+	public void setClassuuids(String classuuids) {
+		this.classuuids = classuuids;
+	}
+	public String getClassnames() {
+		return classnames;
+	}
+	public void setClassnames(String classnames) {
+		this.classnames = classnames;
+	}
+
+	public String getUuid() {
+		return uuid;
+	}
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+
+	private String classnames;
+	  
+	public Timestamp getCreate_time() {
+		return create_time;
+	}
+	public void setCreate_time(Timestamp create_time) {
+		this.create_time = create_time;
+	}
+	public String getCreate_user() {
+		return create_user;
+	}
+	public void setCreate_user(String create_user) {
+		this.create_user = create_user;
+	}
+	public String getCreate_useruuid() {
+		return create_useruuid;
+	}
+	public void setCreate_useruuid(String create_useruuid) {
+		this.create_useruuid = create_useruuid;
+	}
+	public Integer getIsimportant() {
+		return isimportant;
+	}
+	public void setIsimportant(Integer isimportant) {
+		this.isimportant = isimportant;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public Integer getType() {
+		return type;
+	}
+	public void setType(Integer type) {
+		this.type = type;
+	}
+	public String getGroupuuid() {
+		return groupuuid;
+	}
+	public void setGroupuuid(String groupuuid) {
+		this.groupuuid = groupuuid;
+	}
+	  
+
+
+}
