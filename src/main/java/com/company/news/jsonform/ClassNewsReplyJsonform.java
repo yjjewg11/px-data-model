e08f854faa8ed0c48b0060b736bae5baf2758833
yjjewg11 <yjjewg11>
2015-06-25 11:27:58 +0800

    u

--- src/main/java/com/company/news/jsonform/ClassNewsReplyJsonform.java
+++ src/main/java/com/company/news/jsonform/ClassNewsReplyJsonform.java
@@ -5,6 +5,7 @@ package com.company.news.jsonform;
 
 
 
+
 /**
  * 用户注册，body传入json数据转对象。
  * 
@@ -14,19 +15,29 @@ package com.company.news.jsonform;
 public class ClassNewsReplyJsonform extends AbstractJsonform {
 
 
-	private String classuuid;// 品牌名称
+	private String newsuuid;// 品牌名称
+	private String uuid;// 品牌名称
 
+	public String getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
 
 	private String content;// 公司全称
+	private String create_user;// 品牌名称
 
-	public String getClassuuid() {
-		return classuuid;
+	private String create_useruuid;// 公司全称
+
+	public String getNewsuuid() {
+		return newsuuid;
 	}
 
-	public void setClassuuid(String classuuid) {
-		this.classuuid = classuuid;
+	public void setNewsuuid(String newsuuid) {
+		this.newsuuid = newsuuid;
 	}
-
 
 	public String getContent() {
 		return content;
@@ -34,6 +45,22 @@ public class ClassNewsReplyJsonform extends AbstractJsonform {
 
 	public void setContent(String content) {
 		this.content = content;
+	}
+
+	public String getCreate_user() {
+		return create_user;
+	}
+
+	public void setCreate_user(String create_user) {
+		this.create_user = create_user;
+	}
+
+	public String getCreate_useruuid() {
+		return create_useruuid;
+	}
+
+	public void setCreate_useruuid(String create_useruuid) {
+		this.create_useruuid = create_useruuid;
 	}
 	
 
