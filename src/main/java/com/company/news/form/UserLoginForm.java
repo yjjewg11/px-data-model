c873aabce0590ef1432851e843c81259ac1ca932
yjjewg11 <41049630@qq.com>
2015-07-02 16:59:19 +0800

    UserLoginForm 增加grouptype

--- src/main/java/com/company/news/form/UserLoginForm.java
+++ src/main/java/com/company/news/form/UserLoginForm.java
@@ -3,7 +3,14 @@ package com.company.news.form;
 public class UserLoginForm {
   private String loginname;//登录用户名
   private String password;//登录密码
-  public String getLoginname() {
+  private String grouptype;//登录标志,如果是grouptype=0表示,后台管理员.
+  public String getGrouptype() {
+	return grouptype;
+}
+public void setGrouptype(String grouptype) {
+	this.grouptype = grouptype;
+}
+public String getLoginname() {
     return loginname;
   }
   public void setLoginname(String loginname) {
