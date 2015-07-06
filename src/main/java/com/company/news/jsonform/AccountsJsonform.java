035a4b9e7b540f7e54d97be6934ea09967453e71
yjjewg11 <yjjewg11>
2015-07-06 10:00:56 +0800

    u

---
+++ src/main/java/com/company/news/jsonform/AccountsJsonform.java
@@ -0,0 +1,78 @@
+package com.company.news.jsonform;
+
+
+/**
+ * 用户注册，body传入json数据转对象。
+ * 
+ * @author Administrator
+ * 
+ */
+public class AccountsJsonform extends AbstractJsonform {
+
+	private String uuid;// 品牌名称
+
+	private String classuuid;// 品牌名称
+	private String groupuuid;// 品牌名称
+	private String title;// 品牌名称
+
+	private String description;// 公司全称
+	private String accounts_timeStr;
+	private Double num;
+
+	public String getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+
+	public String getClassuuid() {
+		return classuuid;
+	}
+
+	public void setClassuuid(String classuuid) {
+		this.classuuid = classuuid;
+	}
+
+	public String getGroupuuid() {
+		return groupuuid;
+	}
+
+	public void setGroupuuid(String groupuuid) {
+		this.groupuuid = groupuuid;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getAccounts_timeStr() {
+		return accounts_timeStr;
+	}
+
+	public void setAccounts_timeStr(String accounts_timeStr) {
+		this.accounts_timeStr = accounts_timeStr;
+	}
+
+	public Double getNum() {
+		return num;
+	}
+
+	public void setNum(Double num) {
+		this.num = num;
+	}
+
+}
