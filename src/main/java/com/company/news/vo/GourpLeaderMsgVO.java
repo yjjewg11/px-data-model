0b320f9fe46dc2c778300088a7ac00804e0f02fb
yjjewg11 <41049630@qq.com>
2015-07-26 22:21:43 +0800

    add

---
+++ src/main/java/com/company/news/vo/GourpLeaderMsgVO.java
@@ -0,0 +1,42 @@
+package com.company.news.vo;
+
+import java.io.Serializable;
+
+public class GourpLeaderMsgVO implements Serializable {
+
+	private String revice_useruuid;
+	private String revice_user;
+	private String send_useruuid;
+	private String send_user;
+	private String count;
+	public String getRevice_useruuid() {
+		return revice_useruuid;
+	}
+	public void setRevice_useruuid(String revice_useruuid) {
+		this.revice_useruuid = revice_useruuid;
+	}
+	public String getRevice_user() {
+		return revice_user;
+	}
+	public void setRevice_user(String revice_user) {
+		this.revice_user = revice_user;
+	}
+	public String getSend_useruuid() {
+		return send_useruuid;
+	}
+	public void setSend_useruuid(String send_useruuid) {
+		this.send_useruuid = send_useruuid;
+	}
+	public String getSend_user() {
+		return send_user;
+	}
+	public void setSend_user(String send_user) {
+		this.send_user = send_user;
+	}
+	public String getCount() {
+		return count;
+	}
+	public void setCount(String count) {
+		this.count = count;
+	}
+}
---
+++ src/main/java/com/company/news/vo/TeacherPhone.java
@@ -0,0 +1,39 @@
+package com.company.news.vo;
+
+/**
+ * 老师通讯录
+ * @author liumingquan
+ *
+ */
+public class TeacherPhone {
+	//关联老师uuid.type=0,表示院长信息,填写幼儿园uuid.
+	private String teacher_uuid;
+	private String name;
+	private Integer type;//类型,0:院长.1:老师.
+	private String tel;
+	private String getTeacher_uuid() {
+		return teacher_uuid;
+	}
+	public void setTeacher_uuid(String teacher_uuid) {
+		this.teacher_uuid = teacher_uuid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getType() {
+		return type;
+	}
+	public void setType(Integer type) {
+		this.type = type;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	
+}
