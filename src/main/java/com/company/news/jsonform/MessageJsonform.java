48a7bc44ae23f5fef1c23617fbe67164ab8cb1f2
yjjewg11 <yjjewg11>
2015-07-29 09:40:36 +0800

    新增收藏功能

--- src/main/java/com/company/news/jsonform/MessageJsonform.java
+++ src/main/java/com/company/news/jsonform/MessageJsonform.java
@@ -27,6 +27,26 @@ public class MessageJsonform extends AbstractJsonform {
 	  private String message;//电话
 	  
 	  private Integer type;//
+	  
+	  private String group_uuid;//机构uuid
+	  
+	  private String group_name;//机构名
+
+	public String getGroup_uuid() {
+		return group_uuid;
+	}
+
+	public void setGroup_uuid(String group_uuid) {
+		this.group_uuid = group_uuid;
+	}
+
+	public String getGroup_name() {
+		return group_name;
+	}
+
+	public void setGroup_name(String group_name) {
+		this.group_name = group_name;
+	}
 
 	public String getSend_user() {
 		return send_user;
