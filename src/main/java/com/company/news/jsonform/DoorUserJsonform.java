c8997489b99b67e21b66367e5195c48c2ff0d311
yjjewg11 <yjjewg11>
2015-07-29 16:59:22 +0800

    接口支持外部通过身份证号和机构UUID，进行自动绑定门禁卡

---
+++ src/main/java/com/company/news/jsonform/DoorUserJsonform.java
@@ -0,0 +1,64 @@
+package com.company.news.jsonform;
+
+
+/**
+ * 用户注册，body传入json数据转对象。
+ * 
+ * @author Administrator
+ * 
+ */
+public class DoorUserJsonform extends AbstractJsonform {
+
+	private String private_key;// 密钥
+	
+	private String groupuuid;// 机构UUID
+
+	private String cardid;// 卡ID
+	
+	private String userName;// 用户名
+	
+	private String idNo;// 身份证
+
+	public String getGroupuuid() {
+		return groupuuid;
+	}
+
+	public void setGroupuuid(String groupuuid) {
+		this.groupuuid = groupuuid;
+	}
+
+	public String getPrivate_key() {
+		return private_key;
+	}
+
+	public void setPrivate_key(String private_key) {
+		this.private_key = private_key;
+	}
+
+	public String getCardid() {
+		return cardid;
+	}
+
+	public void setCardid(String cardid) {
+		this.cardid = cardid;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getIdNo() {
+		return idNo;
+	}
+
+	public void setIdNo(String idNo) {
+		this.idNo = idNo;
+	}
+
+
+
+}
