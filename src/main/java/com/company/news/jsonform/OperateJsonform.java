e91ddd3e07d799c5b6ff4cb2cde216dbb1434958
yjjewg11 <yjjewg11>
2015-10-22 11:21:16 +0800

    增加操作轨迹模块

---
+++ src/main/java/com/company/news/jsonform/OperateJsonform.java
@@ -0,0 +1,63 @@
+package com.company.news.jsonform;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+
+public class OperateJsonform extends AbstractJsonform {
+	private String create_user;// 操作人
+	
+	private String create_useruuid;// 品牌名称
+	
+	public String getCreate_user() {
+		return create_user;
+	}
+	public void setCreate_user(String create_user) {
+		this.create_user = create_user;
+	}
+	public String getCreate_useruuid() {
+		return create_useruuid;
+	}
+	public void setCreate_useruuid(String create_useruuid) {
+		this.create_useruuid = create_useruuid;
+	}
+	private String message;// 
+
+	private String note;// 
+
+	private String groupuuid;// 
+
+	private String studentuuid;// 
+	
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getNote() {
+		return note;
+	}
+	public void setNote(String note) {
+		this.note = note;
+	}
+	public String getGroupuuid() {
+		return groupuuid;
+	}
+	public void setGroupuuid(String groupuuid) {
+		this.groupuuid = groupuuid;
+	}
+	public String getStudentuuid() {
+		return studentuuid;
+	}
+	public void setStudentuuid(String studentuuid) {
+		this.studentuuid = studentuuid;
+	}
+
+
+
+}
